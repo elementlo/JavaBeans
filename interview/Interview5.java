6d1209de1b2bbbdb7e299459026707f4f7a4be4a
element <element.zhan@gmail.com>
2019-09-05 11:36:45 +0800

    1.修改剑指offer题的提交格式 2.加入题目面试5

---
+++ interview/Interview5.java
@@ -0,0 +1,42 @@
+/**
+ * Interview5 请实现一个函数，将一个字符串中的每个空格替换成“%20”。 例如，当字符串为We Are
+ * Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ */
+public class Interview5 {
+    public static void main(String[] args) {
+        StringBuffer buffer =new StringBuffer("we are happy");
+        System.out.println(replaceSpace(buffer));
+    }
+    
+    public static String replaceSpace(StringBuffer str){
+        for (int i = 0; i < str.length(); i++) {
+            if (' '==(str.charAt(i))) {
+                str.replace(i, i+1, "%20");
+            }
+        }
+        return str.toString();
+    }
+
+    // public String replaceSpace(StringBuffer str) {
+    //     int s= 0;
+    //     for (int i = 0; i < str.length(); i++) {
+    //         if (" ".equals(str.charAt(i))) {
+    //             s++;
+    //         }
+    //     }
+    //     int l= str.length()+2*s;
+    //     int p=l;
+    //     for (int i = 0; i < 2*s; i++) {
+    //         str.append("");
+    //     }
+        
+    //     for (int i = str.length()-1; i >=0; i--) {
+    //         if (" ".equals(str.charAt(i))) {
+    //             p-=3;
+    //             str.insert(p, "%20");
+    //         }
+    //     }
+    //     return "";
+    // }
+
+}
--- interview/TwoDArray.java
+++ interview/TwoDArray.java
@@ -1,39 +1,35 @@
 import java.util.Scanner;
 
 /**
- * TwoDArray 题目:在一个二维数组中,每一行都按照从左到右递增的顺序排序, 
- * 每一列都按照从上到下递增的顺序排序。请完成一个函数,输入这样的一
+ * TwoDArray 题目:在一个二维数组中,每一行都按照从左到右递增的顺序排序, 每一列都按照从上到下递增的顺序排序。请完成一个函数,输入这样的一
  * 个二维数组和一个整数,判断数组中是否含有该整数。
  */
 public class TwoDArray {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int h = scanner.nextInt();
-        int a[][] = new int[n][n];
+        int[][] a= {{}};
+        Find(0,a);
+    }
+
+    public static boolean Find(int target, int[][] array) {
+        if (array == null || array.length<=0||array[0].length<=0) {
+            return false;
+        }
+        int n = array.length;
         int row = 0;
-        int col=n-1;
-        while (scanner.hasNext()) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    a[i][j] = scanner.nextInt();
-                }
+        int col = n - 1;
+        for (int i = 0; i < 2 * n; i++) {
+            if (row > n - 1 || col < 0) {
+                return false;
             }
-            for (int i = 0; i < 2*n; i++) {
-                if (row>n-1||col<0) {
-                    System.out.println("false");                    
-                    break;
-                }
-                if (h>a[row][col]) {
-                    row++;
-                }else if (h<a[row][col]) {
-                    col--;
-                }else{
-                    System.out.println("true");
-                    break;
-                }
+            if (target > array[row][col]) {
+                row++;
+            } else if (target < array[row][col]) {
+                col--;
+            } else {
+                return true;
             }
         }
+        return false;
     }
 }
