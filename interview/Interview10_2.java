c20dfc24f8a76bea9016c9d6492e304c62822e8b
element <element.zhan@gmail.com>
2019-09-12 12:24:27 +0800

    1. 添加了一种内部类实现单例的方法. 2. 添加剑指offer第10题第二小题.

---
+++ interview/Interview10_2.java
@@ -0,0 +1,29 @@
+/**
+ * Interview10_2
+ */
+public class Interview10_2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Solution {
+        public int JumpFloor(int target) {
+            int a = 1;
+            int b = 2;
+            int c = 0;
+            if (target == 1) {
+                c = a;
+            } else if (target == 2) {
+                c = b;
+            } else {
+                for (int i = 0; i < target-2; i++) {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+            }
+            return c;
+        }
+    }
+}
--- interview/Singleton.java
+++ interview/Singleton.java
@@ -1,17 +1,44 @@
-class Singleton {
-    private static volatile Singleton singleton;
+//写一个单列模式
+// class Singleton {
+//     private static volatile Singleton singleton;
 
-    private Singleton() {
-    };
+//     private Singleton() {
+//     };
 
-    public static Singleton singleIns() {
-        if (singleton == null) {
-            synchronized (Singleton.class) {
-                if (singleton == null) {
-                    singleton = new Singleton();
-                }
-            }
+//     public static Singleton singleIns() {
+//         if (singleton == null) {
+//             synchronized (Singleton.class) {
+//                 if (singleton == null) {
+//                     singleton = new Singleton();
+//                 }
+//             }
+//         }
+//         return singleton;
+//     }
+// }
+
+/**
+ * InnerSingleton 内部类方式实现单例
+ */
+public class Singleton {
+    public static void main(String[] args) {
+        
+    }
+
+    static class SingleIns {
+        private SingleIns() {
         }
-        return singleton;
+
+        public static SingleIns getIns() {
+            return InnerSingleton.INS;
+        }
+
+        /**
+         * InnerSingleton
+         */
+        private static class InnerSingleton {
+
+            private static volatile SingleIns INS = new SingleIns();
+        }
     }
 }
