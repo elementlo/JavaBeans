dd91a29a55eb2c217e7f373a011c306c6a6b123a
element <element.zhan@gmail.com>
2019-09-18 18:38:14 +0800

    添加位操作符的注释.

--- interview/interview10_3.java
+++ interview/interview10_3.java
@@ -1,16 +1,15 @@
 /**
- * interview10_3 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
- * 求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ * interview10_3 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。 求该青蛙跳上一个n级的台阶总共有多少种跳法。
  */
 public class interview10_3 {
 
     public static void main(String[] args) {
-        
+
     }
 
     public class Solution {
         public int JumpFloorII(int target) {
-            return 1 << (target - 1);
+            return 1 << (target - 1);// 1*2^(target-1)
         }
     }
 }
